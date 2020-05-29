6abad9544896220123abaf83e4ffabe5e6871fb3
ChinDowns <webdev@chindowns.com>
2020-05-29 08:12:44 -0700

    Display App content and a Footer Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Footer from "./components/Footer"
 
 function App() {
   return (
@@ -101,10 +102,7 @@ function App() {
 
         <article id="blog"></article>
 
-        <footer id="footer" className="d-inline-flex justify-content-around">
-          <p style="color:lightgrey">chindowns@gmail.com</p>
-          <p style="color:lightgrey;">Copyright &copy;2020</p>
-        </footer>
+
       </main>
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -115,7 +113,7 @@ function App() {
           integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
           crossorigin="anonymous"></script>
         <script src="assets/js/script.js"></script>
-
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer(){
+
+    return(
+        <footer id="footer" className="d-inline-flex justify-content-around">
+            <p style={{ color: 'lightgrey' }}>chindowns@gmail.com</p>
+            <p style={{ color: 'lightgrey' }}>Copyright &copy;2020</p>
+        </footer>
+    )
+}
+
+export default Footer;
